3fd1169336a3b9633d70e540fe6e7e6f9ab0eb21
김승환 <prpn97@gmail.com>
2023-07-25 23:07:50 +0900

    feat: 11번가 api 추가, apiType 구분하여 동적처리, utf-8 인코딩

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -3,8 +3,13 @@ import axios from 'axios';
 import config from '../config/index';
 import { CustomRequest } from '../types/customRequest';
 import { Request,Response,NextFunction } from 'express';
+import { parseString } from 'xml2js';
+import iconv from 'iconv-lite';
+
 const { CLIENT_ID, CLIENT_SECRET } = config.naver;
 const { API_KEY } = config.shop11st;
+
+
 export const getDataToScrap = async(req:Request, res:Response, next:NextFunction)=> {
   try {
     // 웹 페이지에 HTTP GET 요청
@@ -44,29 +49,102 @@ export const getNaverShoppingData = async(req:Request, res:Response, next:NextFu
         'X-Naver-Client-Secret': CLIENT_SECRET,
       },
     });
-    console.log(page)
-    console.log(pageSize)
     const result = response.data.items;
+    // const links = await getLink(result);
 
+    // console.log('1:', links)
+    // const realLink = await getCrUrls(links)
+    // res.json(links);
     res.json(result);
   } catch (error) {
     console.error('Error while fetching data:', error);
     next(error);
   }
 }
+const getLink = async (items: string[]) => {
+  try {
+    const links = items.map((item: string) => item.link);
+    console.log('링크 추출 결과:', links);
+    return links;
+  } catch (error) {
+    console.error('Error while getting links:', error);
+    return { error };
+  }
+};
 
-export const get11stShoppingData = async(req:Request, res:Response, next:NextFunction) =>{
+const getCrUrls = async (links:any) => {
+  const crUrls = [];
+  for (const link of links) {
+    try {
+      const response = await axios.get(link);
+      const html = response.data;
+      const $ = load(html);
+
+      // 링크 안의 crUrl 값을 가져옵니다.
+      const crUrl = $('meta[property="og:url"]').attr('content');
+      if (crUrl) {
+        crUrls.push(crUrl);
+      }
+    } catch (error) {
+      console.error(`링크(${link})의 데이터를 가져오거나 파싱하는 중 오류가 발생했습니다:`, error);
+    }
+  }
+  console.log('2: ',crUrls)
+  return crUrls;
+};
+
+
+
+
+// export const get11stShoppingData = async(req:Request, res:Response, next:NextFunction) =>{
+//   try {
+//     const { keyword } = req.params; //검색어
+//     const apiUrl = `https://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=${API_KEY}&apiCode=ProductSearch&keyword=${keyword}`;
+
+//     const response = await axios.get(apiUrl);
+//     parseString(response.data, (err, result) => {
+//       if (err) {
+//         console.error('Error while parsing XML:', err);
+//         next(err);
+//       } else {
+//         const products = result.ProductSearchResponse.Products[0].Product;
+
+//         console.log(products);
+
+//         res.setHeader('Content-Type', 'application/json; charset=utf-8');
+//         res.json(products);
+//       }
+//     });
+//   } catch (error) {
+//     console.error('Error while fetching data:', error);
+//     next(error);
+//   }
+// }
+export const get11stShoppingData = async (req:Request, res:Response, next:NextFunction) => {
   try {
     const { keyword } = req.params; //검색어
     const apiUrl = `https://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=${API_KEY}&apiCode=ProductSearch&keyword=${keyword}`;
 
-    const response = await axios.get(apiUrl);
-    const products = response.data.ProductSearchResponse.Products.Product;
+    const response = await axios.get(apiUrl, {
+      responseType: 'arraybuffer', // 인코딩을 위해 responseType 설정
+    });
 
-    // 가공된 데이터를 클라이언트에 응답
-    res.json();
+    // 응답 데이터를 EUC-KR 인코딩에서 UTF-8로 변환하여 파싱
+    const utf8ResponseData = iconv.decode(response.data, 'EUC-KR');
+    parseString(utf8ResponseData, (err, result) => {
+      if (err) {
+        console.error('Error while parsing XML:', err);
+        next(err);
+      } else {
+        const products = result.ProductSearchResponse.Products[0].Product;
+
+        // UTF-8 인코딩으로 데이터 전달
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        res.json(products);
+      }
+    });
   } catch (error) {
     console.error('Error while fetching data:', error);
     next(error);
   }
-}
+};
